6e2357a3ca00eb5d27b3ddb0982ef09e5bea31fe
Colin Joyce <Cjoyce1@bsd220.org>
2022-10-13 09:26:31 -0500

    Initial Commit

---
+++ Greeting App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Greeting App
+//
+//  Created by Colin Joyce on 9/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Greeting App")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
